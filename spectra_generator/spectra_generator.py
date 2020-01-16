e82efab5450fb86c6ce217acc954d4fd3965f344
Mateo <mateoibarguen@gmail.com>
2020-01-16 08:05:39 -0800

    Add spectra_generator notebook.

--- spectra_generator/spectra_generator.py
+++ spectra_generator/spectra_generator.py
@@ -6,19 +6,20 @@ Python Port Authors: Steven Bradley, Mateo Ibarguen, Nathan Philliber
 """
 import numpy as np
 
+
 def spectra_generator():
     count = 1
-
     datafile = 'MyTrainingN3K80b'
     max_resonances = 5
     num_columns = 3
     num_channels = 10
     scale = 1
     omega_shift = 10
+
     while True:
-        N = np.floor(np.random.uniform(0, 1) * max_resonances) + 1
-        omega = scale * np.random.rand(1, N) + omega_shift
-        gamma = scale / max_resonances * (1 + np.dot(1.8, np.subtract(np.random.rand(1, N), 0.5))) / 4
+        N = int(np.floor(np.random.uniform(0, 1) * max_resonances) + 2)
+        omega = scale * np.random.randint(1, N) + omega_shift
+        gamma = scale / max_resonances * (1 + np.dot(1.8, np.subtract(np.random.randint(1, N), 0.5))) / 4
 
         phase = np.zeros(shape=(1, N))
         amplitude = np.zeros(shape=(1, N))
@@ -36,9 +37,11 @@ def spectra_generator():
 
         for k in range(num_columns):
             offset = 0
-            for jj in range((k-1) * (num_channels+1), k * num_channels):
-                L = np.zeros(1, n)
-                phase[:] = phase_0[jj,:].reshape(N, 1)
+            for jj in range(k * (num_channels+1), k * num_channels):
+                print(n)
+                L = np.zeros((1, n))
+                print("jj: ", jj)
+                phase[:] = phase_0.T[jj,:].reshape(N, 1)
                 amplitude[:] = amplitude_0[jj, :].reshape(N, 1)
 
                 for i in range(N):
@@ -47,4 +50,10 @@ def spectra_generator():
                 cF = np.abs(L)
                 D = cF ** 2
                 D = (D - np.min(D(rng))) / (np.max(D(rng)) - np.min(D(rng)))
+                print(D)
                 offset = offset + 1
+
+
+if __name__ == '__main__':
+    print('here')
+    spectra_generator()
