4be846c2759e947071b75c0aaef9d23caf39a874
NathanPhilliber <nphilliber@gmail.com>
2020-06-06 11:41:48 -0700

    SpectraPreprocessor documentation

--- models/spectra_preprocessor.py
+++ models/spectra_preprocessor.py
@@ -7,11 +7,24 @@ from keras.utils import to_categorical
 
 
 class SpectraPreprocessor:
+    """
+    Class responsible for managing spectra loaders and transforming data for training.
+    """
 
     def __init__(self, dataset_name, num_channels, num_instances, use_generator=False, load_train=True):
+        """
+        Object constructor for Spectra Preprocessor
+
+        :param dataset_name: string for dataset name
+        :param num_channels: int number of channels to use from data
+        :param num_instances: number of instances of data to use
+        :param use_generator: bool for is to use training generator or not
+        :param load_train: bool for if to load data immediately
+        """
         if load_train:
             self.train_spectra_loader = SpectraLoader(dataset_name=dataset_name, subset_prefix=TRAIN_DATASET_PREFIX, eval_now=not use_generator)
         self.test_spectra_loader = SpectraLoader(dataset_name=dataset_name, subset_prefix=TEST_DATASET_PREFIX, eval_now=not use_generator)
+
         self.datagen_config = json.load(open(os.path.join(DATA_DIR, dataset_name, DATAGEN_CONFIG), "r"))
         self.max_nc = self.datagen_config['num_channels']
         self.num_channels = num_channels
@@ -19,6 +32,12 @@ class SpectraPreprocessor:
         self.num_test_instances = None
 
     def get_data(self, loader):
+        """
+        Return reshaped data from loader.
+
+        :param loader: SpectraLoader
+        :return: X matrix, y vector
+        """
         # TODO: Correct?
         dm = loader.get_dm()
         dm_reshaped = np.array(dm[:self.num_instances])[:, :self.num_channels, :]
@@ -30,27 +49,58 @@ class SpectraPreprocessor:
         return X, y_reshaped
 
     def transform(self):
+        """
+        Transform loaded data.
+
+        :return: train and test sets
+        """
         X_train, y_train = self.transform_train()
         X_test, y_test = self.transform_test()
         return X_train, y_train, X_test, y_test
 
     def transform_train(self):
+        """
+        Get and transform train data
+        :return: X, y
+        """
         X_train, y_train = self.get_data(self.train_spectra_loader)
         return X_train, y_train
 
     def transform_test(self):
+        """
+        Get and transform test data
+        :return: X, y
+        """
         X_test, y_test = self.get_data(self.test_spectra_loader)
         return X_test, y_test
 
     def test_generator(self, batch_size):
+        """
+        Get test generator
+        :param batch_size: size of batch
+        :return: test generator
+        """
         return self._generator(loader=self.test_spectra_loader, transform_func=self.transform_test,
                                batch_size=batch_size)
 
     def train_generator(self, batch_size):
+        """
+        Get train generator
+        :param batch_size: size of batch
+        :return: train generator
+        """
         return self._generator(loader=self.train_spectra_loader, transform_func=self.transform_train,
                                batch_size=batch_size)
 
     def _generator(self, loader, transform_func, batch_size):
+        """
+        Loads sharded data from directory in chunks of batch size.
+
+        :param loader: SpectraLoader
+        :param transform_func: data transformation function
+        :param batch_size: size of batch to use
+        :return:
+        """
         cur_set_i = 0
         files = loader.get_data_files()
         num_files = len(files)
@@ -88,6 +138,11 @@ class SpectraPreprocessor:
                 yield spectra_batch_x, spectra_batch_y
 
     def get_num_test_instances(self):
+        """
+        Find the number of test instances from data directory.
+        TODO: store in dataset config
+        :return: int
+        """
         if self.num_test_instances is not None:
             return self.num_test_instances
 
