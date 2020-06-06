b2460d49096beec2a61efd86eb45e101c2485667
NathanPhilliber <nphilliber@gmail.com>
2020-06-06 11:34:55 -0700

    run_train documentation

--- models/run_train.py
+++ models/run_train.py
@@ -10,9 +10,7 @@ from comet_connection import CometConnection
 from models.evaluator import complete_evaluation, EvaluationReport
 
 
-
 """
-
 A command line program to train our neural networks.
 Use:
    > 'python run_train --help'
@@ -27,10 +25,10 @@ OPTIMIZE_PARAMS = {'algorithm': 'bayes', 'spec': {'metric': 'loss', 'objective':
 GENERATOR_LIMIT = 10000  # The minimum number of data points where fit generator should be used
 
 
-loaded_models = None
+loaded_models = None  # Static var to save dynamically loaded architecture models
 def get_loaded_models():
     """
-    Return the loaded_models global or call the import utility on the networks directory if first time
+    Return the loaded_models global or call the import utility on the networks directory if first time.
     :return: model modules tuples
     """
     global loaded_models
@@ -46,6 +44,12 @@ def main():
 
 
 def get_module(model_module_index):
+    """
+    Get a loaded module from index. Used to load detected architecture files.
+
+    :param model_module_index: int
+    :return: module reference and package name string
+    """
     print('model_module_index: ', model_module_index)
     print('loaded_modules: ', get_loaded_models())
     module, package_name = get_loaded_models()[model_module_index]
@@ -53,39 +57,95 @@ def get_module(model_module_index):
 
 
 def load_model(module, class_name, num_channels, n_max, num_timesteps):
+    """
+    Load model object given model class name and attributes.
+
+    :param module: module reference containing class_name
+    :param class_name: string for model class name
+    :param num_channels: int num channels to use from data
+    :param n_max: max number of peaks in data
+    :param num_timesteps: number of time steps per spectra in data
+    :return: model object instance
+    """
     model_class = getattr(module, class_name)
     model = model_class(num_channels, num_timesteps, n_max)
     return model
 
 
 def load_dataset_info(dataset_name):
+    """
+    Load dataset config given dataset name.
+
+    :param dataset_name:
+    :return:
+    """
     dataset_config = json.load(open(os.path.join(DATA_DIR, dataset_name, DATAGEN_CONFIG), "r"))
     return dataset_config
 
 
 def load_data(dataset_name, dataset_config):
+    """
+    Create Spectra Preprocessor given dataset name and config.
+
+    :param dataset_name: string dataset name
+    :param dataset_config: dict dataset config
+    :return: SpectraPreprocessor
+    """
     use_generator = dataset_config["num_instances"] > GENERATOR_LIMIT
     spectra_pp = SpectraPreprocessor(dataset_name=dataset_name, use_generator=use_generator)
     return spectra_pp
 
 
 def get_prior_config(result_dirname):
+    """
+    Load the json file that is saved with each result.
+
+    :param result_dirname: directory name of result
+    :return: dict for result info
+    """
     result_path = os.path.join(MODEL_RES_DIR, result_dirname)
     result_info = json.load(open(os.path.join(result_path, TRAIN_INFO_FILENAME), "rb"))
     return result_info
 
 
 def initialize_model(dataset_name, model_name, model_module_index, num_channels, num_instances):
+    """
+    Initialize model based on dataset information.
+
+    :param dataset_name: string dataset name
+    :param model_name: string model class name
+    :param model_module_index: index of loaded module
+    :param num_channels: int number of channels in data to use
+    :param num_instances: int number of spectra
+    :return: dict dataset config and model object instance
+    """
     dataset_config = load_dataset_info(dataset_name)
+
     # Adjust num_channels to user-specified num_channels instead of the dataset's num_channels.
     dataset_config['num_channels'] = num_channels
     dataset_config['num_instances_used'] = num_instances
+
     module, package_name = get_module(model_module_index)
     model = load_model(module, model_name, num_channels, dataset_config['n_max'], dataset_config['num_timesteps'])
+
     return dataset_config, model
 
 
-def train_model(model, dataset_name, dataset_config, batch_size, n_epochs, num_channels, num_instances, compile_dict=None):
+def train_model(model, dataset_name, dataset_config, batch_size, n_epochs,
+                num_channels, num_instances, compile_dict=None):
+    """
+    Start training sequence.
+
+    :param model: model object instance
+    :param dataset_name: string name of dataset
+    :param dataset_config: dict dataset config
+    :param batch_size: int batch size to train with
+    :param n_epochs: int number of epochs to train
+    :param num_channels: int number of channels to use
+    :param num_instances: int number of spectra instances
+    :param compile_dict: dict compilation info
+    :return: model object instance
+    """
     use_generator = dataset_config["num_instances"] > GENERATOR_LIMIT
     print('use_generator: ', use_generator)
     spectra_pp = SpectraPreprocessor(dataset_name=dataset_name, num_channels=num_channels, num_instances=num_instances,
@@ -106,6 +166,16 @@ def train_model(model, dataset_name, dataset_config, batch_size, n_epochs, num_c
 
 
 def get_evaluation_report(model, dataset_name, num_channels, num_instances, labels=None):
+    """
+    Get evaluation report from trained model. (Uses test data only)
+
+    :param model: model object instance
+    :param dataset_name: string dataset name
+    :param num_channels: int number of channels to use from data
+    :param num_instances: int number of instances of spectra in data
+    :param labels: optional list of string to represent class names
+    :return: evaluation report
+    """
     spectra_pp = SpectraPreprocessor(dataset_name=dataset_name, num_channels=num_channels, num_instances=num_instances,
                                      use_generator=False, load_train=False)
     evaluation_report = EvaluationReport(model, spectra_pp, labels)
@@ -113,6 +183,16 @@ def get_evaluation_report(model, dataset_name, num_channels, num_instances, labe
 
 
 def visualize_evaluate_model(model, dataset_name, num_channels, num_instances, directory):
+    """
+    Generate images of trained model and dataset.
+
+    :param model: model object instance
+    :param dataset_name: string dataset name
+    :param num_channels: int number of channels to use
+    :param num_instances: int number of spectra in dataset
+    :param directory: directory to save images in
+    :return:
+    """
     spectra_pp = SpectraPreprocessor(dataset_name=dataset_name, num_channels=num_channels, num_instances=num_instances,
                                      use_generator=False, load_train=False)
     evaluator = EvaluationReport(model, spectra_pp)
@@ -121,6 +201,10 @@ def visualize_evaluate_model(model, dataset_name, num_channels, num_instances, d
 
 
 def prompt_dataset_string():
+    """
+    Generate a string containing list of datasets to choose from.
+    :return: prompt string
+    """
     data_dirs = sorted(os.listdir(DATA_DIR))
     data_dirs = [data_dir for data_dir in data_dirs if os.path.isdir(os.path.join(DATA_DIR, data_dir))]
     msg = ""
@@ -142,6 +226,14 @@ def prompt_dataset_string():
 
 
 def get_dataset_name(ctx, param, dataset_name_or_selection):
+    """
+    Get the name of the dataset from user selection.
+
+    :param ctx: Click context object
+    :param param: Click arg
+    :param dataset_name_or_selection: string with dataset name or int with selection (from prompt)
+    :return: string dataset name
+    """
     data_dirs = sorted(os.listdir(DATA_DIR))
     data_dirs = [data_dir for data_dir in data_dirs if os.path.isdir(os.path.join(DATA_DIR, data_dir))]
 
@@ -162,6 +254,10 @@ def get_dataset_name(ctx, param, dataset_name_or_selection):
 
 
 def prompt_model_string():
+    """
+    Generate a string containing list of model classes to choose from.
+    :return: prompt string
+    """
     list_i = 0
     names = []
     msg = ""
@@ -183,6 +279,15 @@ def prompt_model_string():
 
 
 def get_model_name(ctx, param, model_name_or_selection):
+    """
+    Get the name of the model class from user selection.
+    Side effect: calls load_models which detects and loads model architectures.
+
+    :param ctx: Click context object
+    :param param: Click arg
+    :param model_name_or_selection: string with model name or int with selection (from prompt)
+    :return: string model class name
+    """
     list_i = 0
     names = []
     module_indices = {}
@@ -215,6 +320,11 @@ def get_model_name(ctx, param, model_name_or_selection):
 
 
 def prompt_previous_run(model_name):
+    """
+    Generate a string containing list of trained instances to choose from.
+    :param model_name: name of model class (used to narrow displayed options)
+    :return: prompt string
+    """
     result_dirs = os.listdir(MODEL_RES_DIR)
     result_dirs = sorted([result_dir for result_dir in result_dirs if model_name == os.path.basename(result_dir).split(RESULT_DIR_DELIM)[0]])
 
@@ -228,6 +338,13 @@ def prompt_previous_run(model_name):
 
 
 def get_result_name(model_name, result_name_or_selection):
+    """
+    Get the name of the model class from user selection.
+
+    :param model_name: string of model class name
+    :param result_name_or_selection: string with result directory name or int with selection (from prompt)
+    :return: string model class name
+    """
     result_dirs = os.listdir(MODEL_RES_DIR)
     result_dirs = sorted([result_dir for result_dir in result_dirs if model_name == os.path.basename(result_dir).split(RESULT_DIR_DELIM)[0]])
 
@@ -244,11 +361,16 @@ def get_result_name(model_name, result_name_or_selection):
 
 
 @main.command(name="continue", help="Continue training an existing run")
-@click.option('--model-name', "-m", prompt=prompt_model_string(), callback=get_model_name, default=None)
-@click.option('--num-channels', "-nc", prompt="Number of Channels: ", type=click.IntRange(min=1))
-@click.option('--num-instances', "-ns", prompt="Number of Instances: ", type=click.IntRange(min=1))
-@click.option('--dataset-name', "-d", prompt=prompt_dataset_string(), callback=get_dataset_name, default=None)
-@click.option("--n-epochs", prompt="Number of epochs", default=DEFAULT_N_EPOCHS, type=click.IntRange(min=1))
+@click.option('--model-name', "-m", prompt=prompt_model_string(), callback=get_model_name,
+              default=None, help="model class name string")
+@click.option('--num-channels', "-nc", prompt="Number of Channels: ", type=click.IntRange(min=1),
+              help="number of channels to use in data")
+@click.option('--num-instances', "-ns", prompt="Number of Instances: ", type=click.IntRange(min=1),
+              help="number of spectra instances to use in data")
+@click.option('--dataset-name', "-d", prompt=prompt_dataset_string(), callback=get_dataset_name,
+              default=None, help="dataset name string")
+@click.option("--n-epochs", prompt="Number of epochs", default=DEFAULT_N_EPOCHS,
+              type=click.IntRange(min=1), help="number of epochs to train for")
 def continue_train_model(model_name, num_channels, num_instances, dataset_name, n_epochs, model_module_index=None):
     result_name = get_result_name(model_name, input(prompt_previous_run(model_name) + ": "))  # If you can figure out how to add this to Click args, then please do
 
@@ -281,12 +403,16 @@ def continue_train_model(model_name, num_channels, num_instances, dataset_name, 
 
 
 @main.command(name="evaluate", help="Evaluate an existing run")
-@click.option('--model-name', "-m", prompt=prompt_model_string(), callback=get_model_name, default=None)
-@click.option('--num-channels', "-nc", prompt="Number of Channels: ", type=click.IntRange(min=1))
-@click.option('--num-instances', "-ns", prompt="Number of Instances: ", type=click.IntRange(min=1))
-@click.option('--dataset-name', "-d", prompt=prompt_dataset_string(), callback=get_dataset_name, default=None)
+@click.option('--model-name', "-m", prompt=prompt_model_string(), callback=get_model_name,
+              default=None, help="model class name string")
+@click.option('--num-channels', "-nc", prompt="Number of Channels: ", type=click.IntRange(min=1),
+              help="number of channels to use in data")
+@click.option('--num-instances', "-ns", prompt="Number of Instances: ", type=click.IntRange(min=1),
+              help="number of spectra instances to use in data")
+@click.option('--dataset-name', "-d", prompt=prompt_dataset_string(), callback=get_dataset_name,
+              default=None, help="dataset name string")
 @click.option('--num-examples', "-d", prompt="Number of examples per peak to visualize predictions for.",
-              default=0, type=click.IntRange(min=0))
+              default=0, type=click.IntRange(min=0), help="number of images to generate per peak class")
 def run_evaluate_model(model_name, num_channels, num_instances, dataset_name, num_examples, model_module_index=None):
     result_name = get_result_name(model_name, input(prompt_previous_run(model_name) + ": "))
     print("Using dataset:", dataset_name)
@@ -327,14 +453,22 @@ def run_evaluate_model(model_name, num_channels, num_instances, dataset_name, nu
 
 
 @main.command(name="new", help="Train a new model")
-@click.option('--model-name', "-m", prompt=prompt_model_string(), callback=get_model_name, default=None)
-@click.option('--dataset-name', "-d", prompt=prompt_dataset_string(), callback=get_dataset_name, default=None)
-@click.option('--num-channels', "-nc", prompt="Number of Channels: ", type=click.IntRange(min=1))
-@click.option('--num-instances', "-ns", prompt="Number of Instances: ", type=click.IntRange(min=1))
-@click.option("--batch-size", "-bs", prompt="Batch size", default=DEFAULT_BATCH_SIZE, type=click.IntRange(min=1))
-@click.option("--n-epochs", "-n", prompt="Number of epochs", default=DEFAULT_N_EPOCHS, type=click.IntRange(min=1))
-@click.option('--use-comet/--no-comet', is_flag=True, default=True)
-@click.option("--comet-name", "-cn", prompt="What would you like to call this run on comet?", default=f"model-{str(datetime.now().strftime('%m%d.%H%M'))}")
+@click.option('--model-name', "-m", prompt=prompt_model_string(), callback=get_model_name,
+              default=None, help="model class name string")
+@click.option('--dataset-name', "-d", prompt=prompt_dataset_string(), callback=get_dataset_name, default=None,
+              help="dataset name string")
+@click.option('--num-channels', "-nc", prompt="Number of Channels: ", type=click.IntRange(min=1),
+              help="number of channels to use in data")
+@click.option('--num-instances', "-ns", prompt="Number of Instances: ", type=click.IntRange(min=1),
+              help="number of spectra instances to use in data")
+@click.option("--batch-size", "-bs", prompt="Batch size", default=DEFAULT_BATCH_SIZE, type=click.IntRange(min=1),
+              help="size of training batch")
+@click.option("--n-epochs", "-n", prompt="Number of epochs", default=DEFAULT_N_EPOCHS, type=click.IntRange(min=1),
+              help="number of epochs to train for")
+@click.option('--use-comet/--no-comet', is_flag=True, default=True,
+              help="flag to determine if commet.ml logging should be used")
+@click.option("--comet-name", "-cn", prompt="What would you like to call this run on comet?",
+              default=f"model-{str(datetime.now().strftime('%m%d.%H%M'))}", help="name to call comet experiment")
 def train_new_model(comet_name, num_channels, num_instances, batch_size, n_epochs, dataset_name, model_name, use_comet, model_module_index=None):
     print("Using dataset:", dataset_name)
     print("Using model:", model_name)
@@ -356,13 +490,20 @@ def train_new_model(comet_name, num_channels, num_instances, batch_size, n_epoch
 
 
 @main.command(name="optimize", help="Optimize model")
-@click.option("--max-n", prompt="Maximum number of experiments: ", default=0, type=click.IntRange())
-@click.option('--model-name', "-m", prompt=prompt_model_string(), callback=get_model_name, default=None)
-@click.option('--dataset-name', "-d", prompt=prompt_dataset_string(), callback=get_dataset_name, default=None)
-@click.option("--batch-size", prompt="Batch size", default=DEFAULT_BATCH_SIZE, type=click.IntRange(min=1))
-@click.option("--n-epochs", prompt="Number of epochs", default=DEFAULT_N_EPOCHS, type=click.IntRange(min=1))
-@click.option('--use-comet/--no-comet', is_flag=True, default=True)
-@click.option("--comet-name", "-cn", prompt="What would you like to call this run on comet?", default=f"model-{str(datetime.now().strftime('%m%d.%H%M'))}")
+@click.option("--max-n", prompt="Maximum number of experiments: ", default=0, type=click.IntRange(),
+              help="max number of peaks")
+@click.option('--model-name', "-m", prompt=prompt_model_string(), callback=get_model_name, default=None,
+              help="model class name string")
+@click.option('--dataset-name', "-d", prompt=prompt_dataset_string(), callback=get_dataset_name,
+              default=None, help="dataset name string")
+@click.option("--batch-size", prompt="Batch size", default=DEFAULT_BATCH_SIZE,
+              type=click.IntRange(min=1), help="size of training batch")
+@click.option("--n-epochs", prompt="Number of epochs", default=DEFAULT_N_EPOCHS,
+              type=click.IntRange(min=1), help="number of epochs to train for")
+@click.option('--use-comet/--no-comet', is_flag=True, default=True,
+              help="flag to determine if commet.ml logging should be used")
+@click.option("--comet-name", "-cn", prompt="What would you like to call this run on comet?",
+              default=f"model-{str(datetime.now().strftime('%m%d.%H%M'))}", help="name to call comet experiment")
 def optimize(max_n, model_name, dataset_name, batch_size, n_epochs, use_comet, comet_name, model_module_index=None):
     dataset_config, model = initialize_model(dataset_name, model_name, model_module_index)
     rocket = None
@@ -387,6 +528,12 @@ def optimize(max_n, model_name, dataset_name, batch_size, n_epochs, use_comet, c
 
 
 def get_params_range(model):
+    """
+    Get the parameter ranges if provided and return ranges.
+
+    :param model: model object instance
+    :return: dict for model parameters
+    """
     model_params = OPTIMIZE_PARAMS
     model_params['parameters'] = {k: v for k, v in model.params_range.items() if k not in set('default')}
     return model_params
