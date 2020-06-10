2b319f4269607b2475e9e9c9b36780ab926ecafe
Mateo <mateoibarguen@gmail.com>
2020-06-10 09:47:45 -0700

    Add run-gen documentation.

--- datagen/spectra_generator.py
+++ datagen/spectra_generator.py
@@ -10,17 +10,19 @@ from abc import abstractmethod, ABC
 from s3 import S3
 
 
-def adjust_peak_locations(peak_locations):
-    if type(peak_locations) == float:
-        return list([peak_locations])
-    return [list(p) for p in peak_locations]
-
-
 def get_num_timesteps(spectrum):
+    """
+
+    :param spectrum:
+    :return:
+    """
     return spectrum.get_num_timesteps()
 
 
 class SpectraGenerator(ABC):
+    """
+
+    """
     DEFAULT_N_MAX = 5.0
     DEFAULT_N_MAX_S = 5.0
     DEFAULT_NC = 10.0
@@ -36,6 +38,20 @@ class SpectraGenerator(ABC):
     def __init__(self, matlab_script=DEFAULT_MATLAB, n_max=DEFAULT_N_MAX, n_max_s=DEFAULT_N_MAX_S, nc=DEFAULT_NC,
                  scale=DEFAULT_SCALE, omega_shift=DEFAULT_OMEGA_SHIFT, dg=DEFAULT_DG, dgs=DEFAULT_DGS,
                  gamma_amp_factor=DEFAULT_GAMMA_AMP_FACTOR, amp_factor=DEFAULT_AMP_FACTOR, epsilon2=DEFAULT_EPSILON2):
+        """
+
+        :param matlab_script: str The matlab script used to generate the data.
+        :param n_max: int The maximum number of possible liquid modes per window in script used to generate data.
+        :param n_max_s: int Maximum number of shell modes in script used to generate data.
+        :param nc: int The number of channels in spectrum data.
+        :param scale: int The scale of the spectrum data.
+        :param omega_shift: float Used for generation of x-axis.
+        :param dg: float Variation in gamma for liquid modes.
+        :param dgs: float Variation in gamma for shell modes.
+        :param gamma_amp_factor: float (optional) Scales gammaAmp: `GammaAmp=scale./(1+0.5*dG)./gammaAmpFactor;`
+        :param amp_factor: float (optional) Scales Amp0S: `Amp0S=rand(nc.*K,NS)./ampFactor;`
+        :param epsilon2: float (optional) Argument that scales white noise: ` D=D + epsilon2.*max(D).*rand(1,omega_res)`
+        """
         self.n_max = float(n_max)
         self.n_max_s = float(n_max_s)
         self.num_channels = float(nc)
@@ -57,6 +73,10 @@ class SpectraGenerator(ABC):
                               'spectra_generator_v2.m': self.engine.spectra_generator_v2}
 
     def generate_spectrum(self):
+        """
+        Uses the matlab script's function to generate the spectrum data based on the class parameters.
+        :return: None
+        """
         matlab_method = self.matlab_mapper[self.matlab_script]
         n, dm, peak_locations, omega_res, n_shell, gamma_amp = matlab_method(float(self.n_max), float(self.n_max_s),
                                               float(self.num_channels), float(self.scale),
@@ -74,9 +94,19 @@ class SpectraGenerator(ABC):
         return spectrum
 
     def generate_spectra(self, n_instances):
-        return [self.generate_spectrum() for i in range(n_instances)]
+        """
+
+        :param n_instances: int The Number of instances to generate.
+        :return: A list of length `n_instances` that stores 'spectrum' objects.
+        """
+        return [self.generate_spectrum() for _ in range(n_instances)]
 
     def generate_spectra_json(self, n_instances):
+        """
+
+        :param n_instances:
+        :return:
+        """
         spectra = self.generate_spectra(n_instances)
         # TODO: Fix this.
         num_timesteps = get_num_timesteps(spectra[0])
@@ -90,12 +120,22 @@ class SpectraGenerator(ABC):
         ...
 
     def generate_save_spectra(self, n_instances, filename):
+        """
+
+        :param n_instances:
+        :param filename:
+        :return:
+        """
         self.num_instances = n_instances
         filepath = os.path.join(DATA_DIR, filename)
         spectra_json = self.generate_spectra_json(n_instances)
         self.save_spectra(spectra_json, filepath)
 
     def update_metadata(self):
+        """
+
+        :return:
+        """
         spectra_generator_dict = dict()
         spectra_generator_dict['n_max'] = self.n_max
         spectra_generator_dict['n_max_s'] = self.n_max_s
@@ -115,6 +155,12 @@ class SpectraGenerator(ABC):
         return spectra_generator_dict
 
     def save_metadata(self, directory=".", filename=DATAGEN_CONFIG):
+        """
+
+        :param directory:
+        :param filename:
+        :return:
+        """
         metadata = self.update_metadata()
         info_filename = os.path.join(directory, filename)
         with open(info_filename, 'w') as f:
@@ -147,25 +193,3 @@ class S3SpectraGenerator(SpectraGenerator):
     def save_spectra(self, spectra_json, filename):
         self.uploader.upload_json(json.dumps(spectra_json), self.update_metadata(), filename)
 
-
-if __name__ == '__main__':
-    os.chdir(GEN_DIR)
-    engine = matlab.engine.start_matlab()
-    matlab_method = engine.spectra_generator_v2
-    n_max = 4.0
-    n_max_s = 5.0
-    num_channels = 1.0
-    scale = 1.0
-    omega_shift = 10.0
-    dg = 0.5
-    dgs = 0.5
-    gamma_amp_factor = 4.0
-    amp_factor = 5.0
-    epsilon2 = 0.05
-
-    n, dm, peak_locations, omega_res, n_shell, gamma_amp = matlab_method(float(n_max), float(n_max_s),
-                                          float(num_channels), float(scale),
-                                          float(omega_shift), float(dg),
-                                          float(dgs), float(gamma_amp_factor),
-                                          float(amp_factor), float(epsilon2), nargout=6)
-    print(n_shell)
